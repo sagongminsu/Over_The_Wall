288bce18cc46e0fcf76dda77137cf3a121b20a21
조성우 <chomuscleguy@gmail.com>
2024-01-11 13:56:56 +0900

    initial merge

---
+++ Assets/ScriptableObjects/Scripts/PlayerSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player",menuName ="Charater/Player")]
+public class PlayerSO : ScriptableObject
+{
+    [field: SerializeField] public PlayerGroundData     GroundData  { get; private set; }
+    [field: SerializeField] public PlayerAirData        AirData     { get; private set; }
+}
---
+++ Assets/_Scripts/Characters/Player/StateMachines/PlayerStateMachine.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : StateMachine
+{
+    public Player player { get; }
+
+    //states
+    public PlayerIdleState idleState { get; }
+
+    public Vector2 MovemnetInput { get; set; }
+    public float MovemnetSpeed { get; private set; }
+    public float RotationDamping { get; private set; }
+    public float MovemnetSpeedModifier { get; private set; } = 1f;
+
+    public float JumpForce { get; set; }
+
+    public Transform MainCameraTransform { get; set; }
+
+    public PlayerStateMachine(Player player)
+    {
+        this.player = player;
+
+        idleState = new PlayerIdleState(this);
+
+        MainCameraTransform = Camera.main.transform;
+
+        MovemnetSpeed = player.Data.GroundData.BaseSpeed;
+        RotationDamping = player.Data.GroundData.BaseRotationDamping;
+    }
+
+}
