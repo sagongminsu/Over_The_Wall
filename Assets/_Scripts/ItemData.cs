bf3be188ba3ed1c89e11a549dcf32ce0598d4e7c
ptt0313 <149345231+ptt0313@users.noreply.github.com>
2024-01-15 20:11:31 +0900

    [feat] Json Data
    Json Data Reader

---
+++ Assets/_Scripts/ItemData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Data
+{
+    int ID;
+    string Name;
+    string InteractionName;
+    string Type;
+}
+
+
+
+public class ItemData : MonoBehaviour
+{
+    Data itemData = new Data();
+
+    private void Start()
+    {
+        Data ItemData = JsonUtility.FromJson<Data>(itemData);
+    }
+}
