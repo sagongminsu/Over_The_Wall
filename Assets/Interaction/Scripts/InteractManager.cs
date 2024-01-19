d6aebb8e21b7cfdce70ed6fc7f72dff1ffadb551
ptt0313 <149345231+ptt0313@users.noreply.github.com>
2024-01-19 20:24:22 +0900

    [feat] InputAction, InteractManager
    PlayerInteractState로 상호작용 구현
    InteractManager를 통해 Interaction와 PlayerInteractState 데이터 공유

---
+++ Assets/Interaction/Scripts/InteractManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class InteractionManager : MonoBehaviour
+{
+    private static InteractionManager instance;
+
+    public static InteractionManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameObject("InteractionManager").AddComponent<InteractionManager>();
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public GameObject CurrentInteractGameObject { get; private set; }
+    public IInteraction CurrentInteraction { get; private set; }
+
+    public void SetCurrentInteraction(GameObject interactObject)
+    {
+        CurrentInteractGameObject = interactObject;
+        CurrentInteraction = interactObject?.GetComponent<IInteraction>();
+    }
+}
--- Assets/_Scripts/Characters/Player/StateMachines/PlayerInteractState.cs
+++ Assets/_Scripts/Characters/Player/StateMachines/PlayerInteractState.cs
@@ -1,19 +1,29 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerInteractState : PlayerGroundedState
 {
+    private InteractionManager interactionManager;
+
     public PlayerInteractState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
     {
+        interactionManager = InteractionManager.Instance;
     }
 
     public override void Enter()
     {
         stateMachine.MovementSpeedModifier = 0;
 
-        Debug.Log("ON");
+        if (interactionManager.CurrentInteractGameObject != null && interactionManager.CurrentInteraction != null)
+        {
+            interactionManager.CurrentInteraction.OnInteract();
+        }
+        else
+        {
+            Debug.LogError("Interaction failed. Check if the object implements IInteraction interface.");
+        }
 
         base.Enter();
-        //enter 애니메이션 넣을거면 넣으면됨
     }
 
     public override void Exit()
@@ -23,7 +33,7 @@ public class PlayerInteractState : PlayerGroundedState
         base.Exit();
         //exit 애니메이션 넣을거면 넣으면됨
     }
-
+    
     public override void Update()
     {
         base.Update();
