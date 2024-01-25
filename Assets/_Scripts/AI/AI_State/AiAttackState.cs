9ef1ee7394ef9c83d81e59b9bc9f2d31604fd3ba
사공민수 <149641823+sagongminsu@users.noreply.github.com>
2024-01-25 20:03:12 +0900

    Add
    Ai

---
+++ Assets/_Scripts/AI/AI_State/AiAttackState.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiAttackState : AiBaseState
+{
+    private bool alreadyAppliedForce;
+
+    public AiAttackState(AiStateMachine aiStateMachine) : base(aiStateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        stateMachine.MovementSpeedModifier = 0;
+        base.Enter();
+        StartAnimation(stateMachine.Ai.AnimationData.AttackParameterHash);
+        //StartAnimation(stateMachine.Ai.AnimationData.BaseAttackParameterHash);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        StopAnimation(stateMachine.Ai.AnimationData.AttackParameterHash);
+        //StopAnimation(stateMachine.Ai.AnimationData.BaseAttackParameterHash);
+
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        ForceMove();
+
+        float normalizedTime = GetNormalizedTime(stateMachine.Ai.Animator, "Attack");
+        if (normalizedTime < 1f)
+        {
+            if (normalizedTime >= stateMachine.Ai.Data.ForceTransitionTime)
+                TryApplyForce();
+
+        }
+        else
+        {
+            if (IsInChaseRange())
+            {
+                stateMachine.ChangeState(stateMachine.ChasingState);
+                return;
+            }
+            else
+            {
+                stateMachine.ChangeState(stateMachine.IdlingState);
+                return;
+            }
+        }
+
+    }
+
+    private void TryApplyForce()
+    {
+        if (alreadyAppliedForce) return;
+        alreadyAppliedForce = true;
+
+        stateMachine.Ai.ForceReceiver.Reset();
+
+        stateMachine.Ai.ForceReceiver.AddForce(stateMachine.Ai.transform.forward * stateMachine.Ai.Data.Force);
+
+    }
+}
