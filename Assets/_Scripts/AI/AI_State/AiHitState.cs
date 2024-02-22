2d694552e6a1df203d1269e6a85743756bfbcfd8
사공민수 <149641823+sagongminsu@users.noreply.github.com>
2024-02-23 01:22:23 +0900

    Add[Ai]
    2.Ai가 대미지를 받으면 데미지 받은 모션추가

--- Assets/_Scripts/AI/AI_State/AiHitState.cs
+++ Assets/_Scripts/AI/AI_State/AiHitState.cs
@@ -4,35 +4,26 @@ using UnityEngine;
 
 public class AiHitState : AiBaseState
 {
-    private static readonly int hitTrigger = Animator.StringToHash("Hit");
+    private static readonly int HitAnimHash = Animator.StringToHash("Hit");
 
-    public AiHitState(AiStateMachine aiStateMachine) : base(aiStateMachine) { }
+    public AiHitState(AiStateMachine aiStateMachine) : base(aiStateMachine)
+    {
+    }
 
     public override void Enter()
     {
-
-        stateMachine.MovementSpeedModifier = 0f;
-
         base.Enter();
-        stateMachine.Ai.Animator.SetTrigger(stateMachine.Ai.AnimationData.HitParameterHash);
-        // 피격 애니메이션 재생 시간이나 다른 조건을 기반으로 다음 상태로의 전환을 계획
-        // 예를 들어, 애니메이션이 끝나면 다음 상태로 전환
-
-
-
-    }
-    public override void Exit()
-    {
-        base.Exit();
-        StopAnimation(stateMachine.Ai.AnimationData.HitParameterHash);
-
-
+        // 피격 애니메이션 재생
+        stateMachine.Ai.Animator.SetTrigger(HitAnimHash);
+        // 추가적으로, 피격 상태에 필요한 초기화나 설정을 여기서 수행
     }
 
     public override void Update()
     {
         base.Update();
-        // 피격 상태에서 수행할 로직을 추가
-        //애니메이션의 진행 정도를 체크하고 완료되면 다음 상태로 전환
+
+
+            stateMachine.ChangeState(stateMachine.IdleingState); // 상태 전환 예시
+        
     }
 }
