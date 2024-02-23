8d168b634f4aaa030211628d49f4e25b929bbc76
ptt0313 <149345231+ptt0313@users.noreply.github.com>
2024-02-23 17:51:22 +0900

    [feat] add NPC dialogue,CompleteQuest UI
    죄수NPC 대화추가
    퀘스트 완료시 퀘스트창에 완료표시 추가

--- Assets/_Scripts/Dialogue/BossNPCDialogue.cs
+++ Assets/_Scripts/Dialogue/BossNPCDialogue.cs
@@ -6,7 +6,7 @@ public class BossNPCDialogue : NPCDialogue
     {
         dialogues = new string[]
         {
-            "이번에 온 신입이야?",
+            "이번에 온 신입인가?",
             "우리가 계획 중인 일이 있는데 부탁 하나 들어주면 너도 끼워주도록 하지.",
             "우리 동료가 갇혀있는 방의 열쇠를 찾아서 내일까지 가져올 수 있겠나?"
         };
