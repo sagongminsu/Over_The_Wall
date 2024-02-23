d1388f21326d24ca775b223706d8eafd58f3e03e
사공민수 <149641823+sagongminsu@users.noreply.github.com>
2024-02-23 18:43:29 +0900

    Fix
    1.Player다시하기 버튼수정

--- Assets/_Scripts/UI/DeathButton.cs
+++ Assets/_Scripts/UI/DeathButton.cs
@@ -3,22 +3,48 @@ using UnityEngine.SceneManagement;
 
 public class DeathButton : MonoBehaviour
 {
-    public GameObject deathPanel; // Inspector에서 할당
+    public GameObject deathPanel;
 
-    // 사망 UI 패널을 활성화하는 함수
+
     public void ShowDeathPanel()
     {
         deathPanel.SetActive(true);
+        UnlockCursor();
+        PauseGame();
     }
-    // 현재 게임 씬을 재시작
+    private void PauseGame()
+    {
+        Time.timeScale = 0; 
+    }
+
+    public void ResumeGame()
+    {
+        deathPanel.SetActive(false);
+        LockCursor();
+        Time.timeScale = 1;
+    }
+    private void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None; 
+        Cursor.visible = true; 
+    }
     public void RestartGame()
     {
+        LockCursor();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     // 메인 메뉴 씬으로 돌아갑니다.
-    public void LoadMainMenu()
+    public void LoadMainScene()
     {
-        SceneManager.LoadScene("MainMenu");
+        LockCursor();
+        SceneManager.LoadScene("Mainscene");
     }
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked; 
+        Cursor.visible = false; 
+    }
+
 }
