a1edc19354554016b43b243bd01a210df047b701
사공민수 <149641823+sagongminsu@users.noreply.github.com>
2024-02-28 10:45:59 +0900

    Fix
    1.Wire부분 수정

--- Assets/_Scripts/Characters/Conditions/PlayerConditions.cs
+++ Assets/_Scripts/Characters/Conditions/PlayerConditions.cs
@@ -4,6 +4,7 @@ using UnityEngine.Events;
 public interface IDamagable
 {
     void TakeDamage(int damageAmount);
+    void TakePhysicalDamage(int damageAmount);
 }
 
 [System.Serializable]
@@ -130,7 +131,11 @@ void Update()
         }
     }
 
-
+    public void TakePhysicalDamage(int damageAmount)
+    {
+        health.Subtract(damageAmount);
+        onTakeDamage?.Invoke();
+    }
 
 
     public bool CanRun()
