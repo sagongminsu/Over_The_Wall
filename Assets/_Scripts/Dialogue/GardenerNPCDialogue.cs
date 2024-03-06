9bac5223fcad3f792d51adeba49ace656f428482
사공민수 <149641823+sagongminsu@users.noreply.github.com>
2024-03-06 19:14:51 +0900

    Fix

--- Assets/_Scripts/Dialogue/GardenerNPCDialogue.cs
+++ Assets/_Scripts/Dialogue/GardenerNPCDialogue.cs
@@ -5,11 +5,13 @@ using UnityEngine.AI;
 
 public class GardenerNPCDialogue : NPCDialogue
 {
-    public Transform playerTransform; 
-    public Transform targetLocation; 
-    private NavMeshAgent agent; 
+    public Transform playerTransform;
+    public Transform targetLocation; // 대화가 끝난 후 이동할 목표 위치
+    public Transform waitLocation; // 시간 외 대기 위치
+    private NavMeshAgent agent;
     private bool hasInteracted = false;
-    private int lastInteractionDay = -1; 
+    private int lastInteractionDay = -1;
+    public Collider interactionZone; // 플레이어가 있어야 할 특정 공간
 
     void Start()
     {
@@ -17,7 +19,6 @@ public class GardenerNPCDialogue : NPCDialogue
         dialogues = new string[]
         {
             "아침시간이다. 기상!",
-            
         };
     }
 
@@ -31,17 +32,36 @@ public class GardenerNPCDialogue : NPCDialogue
             lastInteractionDay = currentDay; 
         }
 
+<<<<<<< Updated upstream
         
         if (gameManager.I.CheckTime(6, 7) && !hasInteracted)
         {
             
             agent.SetDestination(playerTransform.position);
+=======
+        // 현재 시간이 오전 5시에서 6시 사이인지 확인
+        if (gameManager.I.CheckTime(5, 6))
+        {
+            // 플레이어가 interactionZone 내부에 있는지 확인
+            if (!hasInteracted && interactionZone.bounds.Contains(playerTransform.position))
+            {
+                agent.SetDestination(playerTransform.position);
+            }
+>>>>>>> Stashed changes
         }
 
        
         if (hasInteracted)
         {
+<<<<<<< Updated upstream
             agent.SetDestination(targetLocation.position); 
+=======
+            // 그 외의 시간에는 waitLocation 위치로 이동
+            if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+            {
+                agent.SetDestination(waitLocation.position);
+            }
+>>>>>>> Stashed changes
         }
     }
 
@@ -49,16 +69,27 @@ public class GardenerNPCDialogue : NPCDialogue
     {
         base.StartDialogue();
         dialogueText.text = dialogues[0];
+        hasInteracted = true; // 대화를 시작하면 상호작용했다고 표시
     }
 
     public override void OnInteract()
     {
         base.OnInteract();
+<<<<<<< Updated upstream
+=======
+        // 대화가 시작되면, hasInteracted를 true로 설정하여 해당 날짜에 다시 상호작용하지 않도록 함
+        hasInteracted = true;
+>>>>>>> Stashed changes
     }
 
     protected override void EndDialogue()
     {
         base.EndDialogue();
+<<<<<<< Updated upstream
+=======
+        // 대화가 끝나면, NPC는 targetLocation으로 이동
+        agent.SetDestination(targetLocation.position);
+>>>>>>> Stashed changes
         hasInteracted = true;
     }
 }
